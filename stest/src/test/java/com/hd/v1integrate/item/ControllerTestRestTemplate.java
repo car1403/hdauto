0163794ca4ec620cdab126372e9398899f0e4869
smartrector <smartrector@gmail.com>
2024-12-31 11:32:22 +0900

    1230

--- stest/src/test/java/com/hd/v1integrate/item/ControllerTestRestTemplate.java
+++ stest/src/test/java/com/hd/v1integrate/item/ControllerTestRestTemplate.java
@@ -1,6 +1,9 @@
 package com.hd.v1integrate.item;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hd.common.exception.ErrorCode;
 import com.hd.v1.item.dto.ItemRequestDto;
 import jakarta.websocket.OnClose;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
+import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -40,20 +46,66 @@ public class ControllerTestRestTemplate {
     @Test
     @DisplayName("Item 등록 정상")
     @Order(1)
-    public void test1(){
+    public void test1() throws JsonProcessingException {
         // given
         String name = "p1";
         Long price = 1000L;
         ItemRequestDto itemRequestDto =
                 ItemRequestDto.builder().name(name).price(price).build();
+        HttpEntity<ItemRequestDto> requestEntity = new HttpEntity<>(itemRequestDto);
         // when
-       ResponseEntity<String> responseEntity  =
-               testRestTemplate.postForEntity(
-                    getBaseUrl("add"),
-                    itemRequestDto,
-                    String.class
-                );
+        ResponseEntity<String> responseEntity =
+                testRestTemplate.exchange(getBaseUrl("add"), HttpMethod.POST,requestEntity,String.class);
+
+//        ResponseEntity<String> responseEntity  =
+//               testRestTemplate.postForEntity(
+//                    getBaseUrl("add"),
+//                    itemRequestDto,
+//                    String.class
+//                );
         // then
         log.info(responseEntity.getBody());
+        JsonNode jsonNode = objectMapper.readTree(responseEntity.getBody());
+        log.info(jsonNode.get("data").get("id").toString());
+        log.info(jsonNode.get("data").get("name").toString());
+        log.info(jsonNode.get("data").get("price").toString());
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(jsonNode.get("data").get("name").asText()).isEqualTo(name);
+        assertThat(jsonNode.get("data").get("price").asLong()).isEqualTo(price);
+
     }
+
+    // Item Name Duplicated Exception
+    @Test
+    @DisplayName("Item Name Duplicated Exception")
+    @Order(2)
+    public void test2() throws JsonProcessingException {
+        // given
+        String name = "p1";
+        Long price = 1000L;
+        ItemRequestDto itemRequestDto =
+                ItemRequestDto.builder().name(name).price(price).build();
+        HttpEntity<ItemRequestDto> requestEntity = new HttpEntity<>(itemRequestDto);
+        // when
+        ResponseEntity<String> responseEntity = testRestTemplate.exchange(getBaseUrl("add"), HttpMethod.POST,requestEntity,String.class);
+
+//        ResponseEntity<String> responseEntity  =
+//               testRestTemplate.postForEntity(
+//                    getBaseUrl("add"),
+//                    itemRequestDto,
+//                    String.class
+//                );
+        // then
+        log.info(responseEntity.getBody());
+        JsonNode jsonNode = objectMapper.readTree(responseEntity.getBody());
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(jsonNode.get("state").asInt()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(jsonNode.get("message").asText()).isEqualTo(ErrorCode.NAME_DUPLICATED.getErrorMessage());
+        assertThat(jsonNode.get("data").get("errorCode").asText()).isEqualTo(ErrorCode.NAME_DUPLICATED.getErrorCode());
+        assertThat(jsonNode.get("data").get("errorMessage").asText()).isEqualTo(ErrorCode.NAME_DUPLICATED.getErrorMessage());
+
+    }
+
 }
